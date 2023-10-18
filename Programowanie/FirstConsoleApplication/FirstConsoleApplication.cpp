7259db1341915a9ae012028fe163ea1fd1cf3de5
MrPlax <aadam.blank@gmail.com>
2023-10-18 08:12:50 +0200

    Zmienne i algorytm

--- Programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
+++ Programowanie/FirstConsoleApplication/FirstConsoleApplication.cpp
@@ -1,5 +1,5 @@
 // FirstConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
-//
+
 
 #include <iostream>
 
---
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+int main()
+{
+	
+}
+
+/*
+
+Algorytm - zbiór instrukcji realizuj¹cy postawione przed nim zadanie
+Sposoby zapisu algorytmu:
+* W punktach
+* Opis s³owny
+* Jêzyk programowania lub pseudokod
+* Schemat blokowy
+
+*/
