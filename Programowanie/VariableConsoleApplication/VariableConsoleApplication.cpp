b98c9a8d85754d7f6bb0efd07e50f4d9353d8c26
MrPlax <aadam.blank@gmail.com>
2023-11-07 11:37:03 +0100

    Dokończenie programu

--- Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
+++ Programowanie/VariableConsoleApplication/VariableConsoleApplication.cpp
@@ -14,20 +14,23 @@ void task1()
 void task2()
 {
 	float  lenghtInMeterFromUser;
-	std::cout << "Podaj d³ugoœæ w metrach: ";
-	std::cin >> lenghtInMeterFromUser"\n";
+	std::cout << "Podaj dlugosc w metrach: ";
+	std::cin >> lenghtInMeterFromUser;
 
-	float lenghtInCentimiter;
-	lenghtInCentimiter = lenghtInMeterFromUser / 100;
-	std::cout << "D³ugoœæ w centymetrach wynosi: " << lenghtInCentimiter"\n";
+	float  lenghtInCentimiterFromUser;
+	lenghtInCentimiterFromUser = lenghtInMeterFromUser * 100;
 
-	float lenghtInMilimiter;
-	lenghtInMilimiter = lenghtInMeterFromUser / 1000;
-	std::cout << "D³ugoœæ w milimetrach wynosi: " << lenghtInMilimiter"\n";
+	float  lenghtInMilimiterFromUser;
+	lenghtInMilimiterFromUser = lenghtInCentimiterFromUser * 10;
 
-	float lenghtInKilometer;
-	lenghtInKilometer = lenghtInMeterFromUser / 1000;
-	std::cout << "D³ugoœæ w kilometrach wynosi: " << lenghtInKilometer"\n";
+	float  lenghtInKilometerFromUser;
+	lenghtInKilometerFromUser = lenghtInMeterFromUser / 1000;
+
+	std::cout << "Metry: " << lenghtInMeterFromUser << "\n";
+	std::cout << "Centymetry: " << lenghtInCentimiterFromUser << "\n";
+	std::cout << "Milimetry: " << lenghtInMilimiterFromUser << "\n";
+	std::cout << "Kilometry: " << lenghtInKilometerFromUser << "\n";
+
 }
 
 
@@ -42,7 +45,7 @@ int main()
 
 /*
 
-Algorytm - zbiór instrukcji realizuj¹cy postawione przed nim zadanie
+Algorytm - skoñczony zbiór instrukcji realizuj¹cy postawione przed nim zadanie. Kolejnoœæ instrukcj w zbiorze jest okreœlona.
 Sposoby zapisu algorytmu:
 * W punktach
 * Opis s³owny
@@ -56,19 +59,19 @@ Deklaracja zmiennej - moment jej utworzenia
 typ_zmiennej nazwa_zmiennej;
 Typ zmiennej - mówi o wielkoœci obszaru w pamiêci i o rodzaju przechowywanych danych
 
-short		liczby ca³kowite ze znakiem		2 bajty -> 16 bitów		<-32768; 32 767>
-int/long	liczby ca³kowite ze znakiem		4 bajty -> 32 bity		<-2 147 483 648; 2 147 483 647> od - 2 miliardów do 2 miliardów 
-long long	liczby ca³kowite ze znakiem		8 bajtów -> 64bity		<-9 223 372 036 854 775 808; 9 223 372 036 854 775 807> -9 trylionów do 9 trylionów 
+short		liczby ca³kowite ze znakiem		2 bajty -> 16 bitów		<-32 768; 32 767>
+int/long	liczby ca³kowite ze znakiem		4 bajty -> 32 bity		<-2 147 483 648; 2 147 483 647> od - 2 miliardów do 2 miliardów
+long long	liczby ca³kowite ze znakiem		8 bajtów -> 64bity		<-9 223 372 036 854 775 808; 9 223 372 036 854 775 807> -9 trylionów do 9 trylionów
 
 Je¿eli przed powy¿szymi typami danych  dodamy s³owo "unsigned" to
-zakres jest od 0 do podwojonego zakresu górnego plus 1 (32 767*2 + 1)
+zakres jest od 0 do podwojonego zakresu górnego plus 1 (short->32 767*2 + 1)
 
 float			liczby rzeczywiste ze znakiem 4 bajty
 double			liczby rzeczywiste ze znakiem 8 bajtów
 long double		liczby rzeczywiste ze znakiem 12 bajtów
 
 Nazwa zmiennej jest to nazwa obszaru pamiêci. Poprzez tê nazwê odwo³ujemy siê do tego obszaru.
-Wymagania kompilatora wzglêdem nazwy: 
+Wymagania kompilatora wzglêdem nazwy:
 * u¿ycie tylko dozwolonych znaków - alfabet angielsk a-z oraz A-Z
 * cyfry arabskie	0-9
 * podkreœlenie (pod³oga)_	(nie trzeba u¿ywaæ wszystkiego)
@@ -84,4 +87,4 @@ Wymagania programistów:
 	* zaczynamy z du¿ej litery np.firstNumberFromUser
 - nazwa zmiennej musi oddwaæ charakter przechowywanych danych, czyli mieæ znaczenie
 - nazwy zmiennych po angielsku
-*/ 
+*/
