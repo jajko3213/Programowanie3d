86ab2ebe03ba19a144bcbba573d11f71bf3183f0
MrPlax <aadam.blank@gmail.com>
2023-11-28 11:40:18 +0100

    Instrukcje if

---
+++ Programowanie/InstructionIfConsoleApplication/InstructionIfConsoleApplication.cpp
@@ -0,0 +1,48 @@
+// InstructionIfConsoleApplication.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+
+//Napisz program, którypobierze liczbê od u¿ytkownika i wyœwietli czy jest ona dodatnia czy nie.
+void task1()
+{
+    int number;
+    std::cout << "Podaj liczbe, ktorom chcesz sprawdzic: ";  "\n";
+    std::cin >> number;
+    
+    if (number > 0)
+    {
+        std::cout << "Liczba jest dodatnia\n";
+    }
+    else 
+    {
+        std::cout << "Liczba jest ujemna\n";
+    }
+}
+
+
+int main()
+{
+    task1();
+}
+
+/*
+Operatory porównania:
+>   wiêkszoœci
+<   mniejszoœci
+>=  wiêksze lub równe
+<=  mniejsze lub równe
+== równoœæ
+!= rózñoœæ
+*/
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
