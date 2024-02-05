bfba72818be9faf7b9f968f2bc8c9818536c6ae8
MrPlax <aadam.blank@gmail.com>
2024-02-05 09:12:31 +0100

    Gra losująca liczby

--- Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
+++ Programowanie/LoopDoWhileConsoleApplication/LoopDoWhileConsoleApplication.cpp
@@ -39,9 +39,35 @@ void task1()
 	std::cout << "Podano liczbê " << number << "\n";
 }
 
+//Napisz program, który wylosuje liczbê, a nastêpnie u¿ytkownik bêdzie musia³ j¹ odgadn¹æ
+void task2()
+{
+	int randomNumber, numberFromUser, userAttempt;
+
+	userAttempt = 0;
+	srand(time(0));
+	randomNumber = rand() % 101;
+
+	std::cout << "Wylosowano liczbê z zakresu od 0 do 100 odgadnij j¹.\n";
+	
+	do
+	{
+		std::cout << "Podaj liczbê: \n";
+		std::cin >> numberFromUser;
+		userAttempt++;
+
+		if (numberFromUser < randomNumber)
+			std::cout << "Liczba jest za ma³a\n";
+		else if (numberFromUser > randomNumber)
+			std::cout << "Liczba jest za du¿a\n";
+		else
+			std::cout << "Zgad³eœ " << "po " << userAttempt << " prób(ie)ach.\n" << "Wylosowan¹ liczb¹ jest: " << randomNumber << "\n";
+	} while (numberFromUser != randomNumber);
+}
+
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
-	task1();
+	task2();
 }
 
