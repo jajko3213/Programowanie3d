30ddca2794943788c399e6ff6d7786989ac3a928
MrPlax <aadam.blank@gmail.com>
2024-03-04 09:12:31 +0100

    Zadania z pętlą while, wzór na NWD

--- Programowanie/LoopWhileConsoleApplication/LoopWhileConsoleApplication.cpp
+++ Programowanie/LoopWhileConsoleApplication/LoopWhileConsoleApplication.cpp
@@ -1,13 +1,56 @@
 #include <iostream>
 
-//
+//Napisz program, który policzy sumê cyfr podanej przez u¿ytkownika liczby.
 void task1()
 {
+	
+	int number, numberOfDigit = 1;
+	
+	do
+	{
+		std::cout << "Podaj liczbê dodatni¹: \n";
+		std::cin >> number;
+	} while (number < 0);
+	/*
+	if (number > 9)
+	{
+		number = number / 10;
+		numberOfDigit++;
+		if (number > 9)
+		{
+			number = number / 10;
+			numberOfDigit++;
+			if (number > 9)
+			{
+				//...
+			}
+		}
+	}
+	*/
+
+	while (number > 9)
+	{
+		number = number / 10;
+		numberOfDigit++;
+	}
+
+	std::cout << "Iloœæ cyfr w liczbie wynosi: " << numberOfDigit;
+}
+
+//Napisz program, który policzy NWD dwóch liczb.
+void task2()
+{
+	/*
+	 Wzór na NWD = { a , b = 0												}
+				   { NWD(b, a % b), b != 0 (skreœlony znak równoœci, nie !=)}
+	*/
+
 
 }
 
 int main()
 {
-	task1();
+	setlocale(LC_CTYPE, "polish");
+	task2();
 }
 
