2773eefcfd92c3d56c56fef50d7dc3c3e5c86ef7
MrPlax <aadam.blank@gmail.com>
2024-03-06 10:44:19 +0100

    Zadanie

--- Programowanie/LoopWhileConsoleApplication/LoopWhileConsoleApplication.cpp
+++ Programowanie/LoopWhileConsoleApplication/LoopWhileConsoleApplication.cpp
@@ -80,15 +80,33 @@ void task2()
 	std::cout << "NWD = " << firstNumber;
 }
 
-//
+/*
+Miasto T. ma obecnie 100 tys. mieszkañców, ale jego populacja roœnie co roku o 3% rocznie.
+Miasto B. ma 300 tys. mieszkañców i ta liczba roœnie w tempie 2% na rok.
+ Wykonaj symulacjê prezentuj¹c¹ liczbê mieszkañców w obu miastach i zatrzymuj¹c¹ siê,
+ gdy liczba mieszkañców miasta T. przekroczy liczbê z miasta B.
+*/
+
+//W tej pêtli s¹ 2 zmienne streuj¹ce, zale¿y co jest w warunku
 void task3()
 {
-	
+	double tCityPopulation = 100000, bCityPopulation = 300000;
+	int years = 0;
+
+	while (tCityPopulation < bCityPopulation)
+	{
+		tCityPopulation = tCityPopulation * 1.03;
+		bCityPopulation = bCityPopulation * 1.02;
+		years++;
+	}
+
+	std::cout << "Populacja miasta T wynios³a " << tCityPopulation << " po " << years << " latach\n";
+	std::cout << "Populacja miasta B wynios³a " << bCityPopulation << " po " << years << " latach";
 }
 
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
-	task2();
+	task3();
 }
 
