b6522e56832957d065af9ca8ce6b30920edae838
MrPlax <aadam.blank@gmail.com>
2024-03-08 14:41:55 +0100

    Pętla For

---
+++ Programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+//Napisz program, który wyœwietli "Hello World" tyle razy ile chce u¿ytkownik
+void task1()
+{
+
+}
+
+int main()
+{
+    setlocale(LC_CTYPE, "polish");
+    task1();
+}
+
