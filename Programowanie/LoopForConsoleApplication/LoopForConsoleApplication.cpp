d895069e0191721422e3b10e7f237914377fe46f
MrPlax <aadam.blank@gmail.com>
2024-03-08 14:52:41 +0100

    Pętla for zadanie

--- Programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
+++ Programowanie/LoopForConsoleApplication/LoopForConsoleApplication.cpp
@@ -3,7 +3,18 @@
 //Napisz program, który wyœwietli "Hello World" tyle razy ile chce u¿ytkownik
 void task1()
 {
-
+	int number;
+    
+	do
+	{
+		std::cout << "Podaj pierwsz¹ liczbê dodatni¹: \n";
+		std::cin >> number;
+	} while (number < 0);
+    
+	for (int counter = 0; counter != number; counter++)
+	{
+		std::cout << "Hello World\n";
+	}
 }
 
 int main()
