416d7d01167d9ea817d3ecc6b8a5cccd30db5c9b
MrPlax <aadam.blank@gmail.com>
2024-03-27 09:01:21 +0100

    Dodanie CharactersConsoleApplication

---
+++ Programowanie/CharacterConsoleApplication/CharacterConsoleApplication.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+//Napisz program który pobierze znak od u¿ytkownika i wyœwieli go.
+void task1()
+{
+    char characterFromUser;
+    std::cout << "Podaj jeden znak:\n";
+    std::cin >> characterFromUser;
+
+    std::cout << "Podano: " << characterFromUser << "\n";
+
+    characterFromUser = characterFromUser + 1;
+
+    std::cout << "Podano: " << characterFromUser << "\n";
+
+    characterFromUser = 97;
+    characterFromUser = 'a';
+    characterFromUser = 'a' + 1; // 'b'
+}
+
+int main()
+{
+    task1();
+}
