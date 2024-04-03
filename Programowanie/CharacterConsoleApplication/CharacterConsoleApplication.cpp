80aa516ad64ced720b4efa81535598595ac0ce6c
Dawid Strzelecki <strzelecki002@wp.pl>
2024-04-03 21:10:02 +0200

    Zadania

--- Programowanie/CharacterConsoleApplication/CharacterConsoleApplication.cpp
+++ Programowanie/CharacterConsoleApplication/CharacterConsoleApplication.cpp
@@ -18,6 +18,88 @@ void task1()
     characterFromUser = 'a' + 1; // 'b'
 }
 
+// Napisz program, który wczyta znak z klawiatury i sprawdzi czy jest to ma³a litera alfabetu.
+void task2()
+{
+	char characterFromUser;
+	std::cout << "Podaj jeden znak:\n";
+	std::cin >> characterFromUser;
+
+	//wersja 1
+	if (characterFromUser == 'a'
+		|| characterFromUser == 'b'
+		|| characterFromUser == 'c'
+		|| characterFromUser == 'd'
+		//...
+		|| characterFromUser == 'z')
+	{
+		std::cout << "Poda³es ma³¹ literê alfabetu\n";
+	}
+	else
+	{
+		std::cout << "Poda³eœ inny znak\n";
+	}
+
+	//wersja 2
+	if (characterFromUser >= 97
+		&& characterFromUser <= 122)
+	{
+		std::cout << "Poda³es ma³¹ literê alfabetu\n";
+	}
+	else
+	{
+		std::cout << "Poda³eœ inny znak\n";
+	}
+
+	//wersja 3
+	if (characterFromUser >= 'a'
+		&& characterFromUser <= 'z')
+	{
+		std::cout << "Poda³es ma³¹ literê alfabetu\n";
+	}
+	else
+	{
+		std::cout << "Poda³eœ inny znak\n";
+	}
+}
+
+//Napisz program, który poprosi cie o twoje imiê i ciê przywita.
+void task3()
+{
+	std::string userName;
+	std::cout << "Podaj swoje imiê\n";
+	std::cin >> userName;
+
+	std::cout << "Witaj " << userName << " tutaj.\n";
+
+	userName = "To jest nowy tekst";
+}
+
+//Program sprawdzaj¹cy czy podane has³o jest poprawne 
+// (np. jeœli has³o jest "abc123", 
+// program powinien wyœwietliæ "has³o poprawne",
+//  jeœli jest inne, powinien wyœwietliæ "has³o niepoprawne").
+void task4()
+{
+	std::string password;
+	std::cout << "Podaj has³o\n";
+	std::cin >> password;
+
+	if (password == "abc123")
+	{
+		std::cout << "Has³o poprawne\n";
+	}
+	else
+		std::cout << "Has³o niepoprawne\n";
+}
+
+//Napisz program, który wczyta ³añcuch znaków i policzy ile jest ma³ych liter 'a'.
+void task5()
+{
+
+}
+
+
 int main()
 {
     task1();
