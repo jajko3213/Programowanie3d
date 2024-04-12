1a9cb38955effaf48f1af1d196355c8d928d9684
MrPlax <aadam.blank@gmail.com>
2024-04-12 14:15:20 +0200

    zadania własne

--- Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
+++ Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -19,7 +19,43 @@ void task1()
     std::cout << "\n";
 }
 
+//Program, który wype³ni tablice (100 elementow¹) liczbami od 1 do 99, a nastêpnie obliczy sumê oraz œredni¹ liczb parzystych.
+void task2()
+{
+    const unsigned short ARRAY_NUMBER_SIZE = 100;
+    
+
+    int numbers[ARRAY_NUMBER_SIZE];
+    for (int i = 1; i < ARRAY_NUMBER_SIZE; i++)
+    {
+        numbers[i] = i;
+        std::cout << numbers[i] << " ,";
+    }
+
+    std::cout << "\n";
+    
+    int sum = 0, counter = 0;
+    for (int i = 1; i < ARRAY_NUMBER_SIZE; i++)
+    {
+        if (numbers[i] % 2 == 0)
+        {
+            sum = sum + numbers[i];
+            counter++;
+        }
+        
+    }
+    
+    std::cout << "\n";
+
+    std::cout << "Suma liczb parzystych tablicy wynosi: " << sum << "\n";
+    
+    int arithmeticMeanOfEvenNumbers = sum / counter;
+
+    std::cout << "Œrednia arytmetyczna tych liczb wynosi: " << arithmeticMeanOfEvenNumbers;
+}
+
 int main()
 {
     setlocale(LC_CTYPE, "polish");
+    task2();
 }
