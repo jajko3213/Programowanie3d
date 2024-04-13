5420fe50b701938fcc3b32d2f8357f67a10f0a34
Dawid Strzelecki <strzelecki002@wp.pl>
2024-04-13 12:17:33 +0200

    zadanie

--- Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
+++ Programowanie/ArrayConsoleApplication/ArrayConsoleApplication.cpp
@@ -52,8 +52,53 @@ void task2()
     std::cout << "Œrednia arytmetyczna tych liczb wynosi: " << arithmeticMeanOfEvenNumbers;
 }
 
+//Napisz program który przyjmie od u¿ytkownika listê ocen z pewnego przedmiotu i obliczy ich œredni¹ arytmetyczn¹.
+/*Funkcje programu :
+
+*Poproœ u¿ytkownika o wprowadzenie ka¿dej oceny.
+*Zapisz oceny w tablicy.
+*Oblicz œredni¹ arytmetyczn¹ ocen.
+*Wyœwietl œredni¹ ocen na ekranie.
+*/
+
+void task3()
+{
+    std::cout << "Podaj swoje oceny z jednego dowolnego przedmiotu szkolnego, mo¿esz podaæ maksymalnie 10 ocen\n";
+    std::cout << "Napisz z jakiego przedmiotu obliczasz œredni¹. Napisz to w sposób np. historii, biologi, chemi, angielskiego itd.\n";
+    std::string subjectFromUser;
+    std::cin >> subjectFromUser;
+
+    const unsigned int ARRAY_NUMBERS_SIZE = 10;
+    int grades[ARRAY_NUMBERS_SIZE];
+    int sum = 0, counter = 0;
+
+    for (int i = 0; i < ARRAY_NUMBERS_SIZE; i++) {
+        bool grade = false;
+        while (grade == false) 
+        {
+            std::cout << "Podaj swoj¹ " << (i + 1) << " ocenê: ";
+            std::cin >> grades[i];
+            if (grades[i] >= 1 && grades[i] <= 6) 
+            {
+                grade = true;
+                std::cout << "\n";
+                sum = sum + grades[i];
+                counter++;
+            }
+            else 
+            {
+                std::cout << "Podana ocena jest w z³ej skali. Podaj ocenê z zakresu od 1 do 6.\n";
+         
+            }
+        }
+    }
+
+    int arithmeticAverage = sum / counter;
+    std::cout << "Twoja œrednia ocen z " + subjectFromUser <<  " wynosi: " << arithmeticAverage << "\n";
+}
+
 int main()
 {
     setlocale(LC_CTYPE, "polish");
-    task2();
+    task3();
 }
