8f763d484f11a9f68c912ad5823f159b377bc410
MrPlax <aadam.blank@gmail.com>
2024-05-17 14:17:43 +0200

    Funkcja zadanie

--- Programowanie/FunctionConsoleApplication/FunctionConsoleApplication.cpp
+++ Programowanie/FunctionConsoleApplication/FunctionConsoleApplication.cpp
@@ -5,7 +5,7 @@ Funkcja - zbiór instrukcji. Definicja/deklaracja funkcji sk³ada siê z dwóch czêœ
 * nagówek - sposób u¿ycia funkcji
 	- typ zwracany
 	- nazwa (poprawny identyfikator)
-	- parametrów w nawiasie (nieobowi¹z kowe)
+	- parametrów w nawiasie (nieobowi¹zkowe)
 * cia³o - zbiór instrukcji
 
 typZwracany nazwaFuncji(parametry)
@@ -16,11 +16,37 @@ typZwracany nazwaFuncji(parametry)
 
 void showHello()
 {
-	std::cout << "Hello World";
+	std::cout << "Hello World\n";
+}
+
+void showHelloMaciek()
+{
+	std::cout << "Witaj Maciej\n";
+}
+
+void showHelloJan()
+{
+	std::cout << "Witaj Jan\n";
+}
+
+void showWelcome(std::string n)
+{
+	std::cout << "Witaj " << n << "\n";
 }
 
 int main()
 {
 	setlocale(LC_CTYPE, "polish");
 	showHello();
+	//jakiœ kod
+	showHello();
+	showHelloMaciek();
+	showHelloJan();
+	std::cout << "\n";
+	std::string name;
+	name = "Mackiek";
+	showWelcome(name);
+	name = "Jan";
+	showWelcome(name);
+	showWelcome("Anna");
 }
