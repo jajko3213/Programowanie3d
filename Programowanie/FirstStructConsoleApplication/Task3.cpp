58ed9d245ab58c9d75e02ae6f1c5555cc80178be
Uczen <Uczen@DESKTOP-6O2JQB3>
2024-09-25 08:38:25 +0200

    zad

--- Programowanie/FirstStructConsoleApplication/Task3.cpp
+++ Programowanie/FirstStructConsoleApplication/Task3.cpp
@@ -11,7 +11,12 @@ void accountInformation(bankAccount &account)
 {
 	std::cout << "Informacje o koncie bankowym.\n";
 	std::cout << "W³aœciciel: " << account.owner << "\n";
-	std::cout << "Saldo: " << account.balance << " " << account.currency << "\n";
+	std::cout << "Saldo: " << account.balance << " " << account.currency << "\n\n";
+}
+
+void depositToAccount(bankAccount& account, double amount)
+{
+	account.balance = account.balance + amount;
 }
 
 void task3()
@@ -22,4 +27,14 @@ void task3()
 	firstAccount.currency = "z³";
 
 	accountInformation(firstAccount);
+	depositToAccount(firstAccount, 500);
+	accountInformation(firstAccount);
+
+
+	bankAccount secondAccount;
+	secondAccount.balance = 15000;
+	secondAccount.owner = "Ewa Kowalska";
+	secondAccount.currency = "z³";
+
+	accountInformation(secondAccount);
 }
