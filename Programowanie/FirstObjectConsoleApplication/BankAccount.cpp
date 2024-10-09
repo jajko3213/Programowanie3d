765dba9cffc0ebfcbdafa08b5b702e434b8b4e83
q <q>
2024-10-09 08:10:21 +0200

    Podział metody w pliku h na drugi plik cpp (ciało metody musi być w pliku cpp, informacja w pliku h że to metoda poprzez dodanie po typie funkci nazwy klasy przed nazwą metody)

---
+++ Programowanie/FirstObjectConsoleApplication/BankAccount.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include "BankAccount.h"
+
+void BankAccount::AccountInformation()
+{
+	std::cout << "Informacje o koncie bankowym.\n";
+	std::cout << "W³aœciciel: " << owner << "\n";
+	std::cout << "Saldo: " << balance << " " << currency << "\n\n";
+}
+
+void BankAccount::DepositToAccount(double amount)
+{
+	amount = abs(amount);
+	balance = balance + amount;
+}
+
+bool BankAccount::WidthDrawFromAccount(double amount)
+{
+	amount = abs(amount);
+	if (balance - amount >= 0)
+	{
+
+		balance = balance - amount;
+		return true;
+	}
+	return false;
+}
+
+void BankAccount::TransferBetweenAccounts(BankAccount& targetAccount, double amount)
+{
+	if (WidthDrawFromAccount(amount) == true)
+		targetAccount.DepositToAccount(amount);
+}
--- Programowanie/FirstObjectConsoleApplication/BankAccount.h
+++ Programowanie/FirstObjectConsoleApplication/BankAccount.h
@@ -8,34 +8,11 @@ public:
 	std::string owner; //w³aœciciel
 	std::string currency; // waluta
 
-	void AccountInformation()
-	{
-		std::cout << "Informacje o koncie bankowym.\n";
-		std::cout << "W³aœciciel: " << owner << "\n";
-		std::cout << "Saldo: " << balance << " " << currency << "\n\n";
-	}
-
-	void DepositToAccount(double amount)
-	{
-		amount = abs(amount);
-		balance = balance + amount;
-	}
-
-	bool WidthDrawFromAccount(double amount)
-	{
-		amount = abs(amount);
-		if (balance - amount >= 0)
-		{
-
-			balance = balance - amount;
-			return true;
-		}
-		return false;
-	}
-
-	void TransferBetweenAccounts(BankAccount& targetAccount, double amount)
-	{
-		if (WidthDrawFromAccount(amount) == true)
-			targetAccount.DepositToAccount(amount);
-	}
+	void AccountInformation();
+	
+	void DepositToAccount(double amount);
+	
+	bool WidthDrawFromAccount(double amount);
+	
+	void TransferBetweenAccounts(BankAccount& targetAccount, double amount);
 };
