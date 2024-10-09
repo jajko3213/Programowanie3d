f5b92d4503980df2c9ef1d917d53607666ea453a
q <q>
2024-10-09 08:55:36 +0200

    Napisanie funkcji zapisującej do pliku txt informacji

--- Programowanie/ReadAndWriteOnFilesConsoleApplication/ReadAndWriteOnFilesConsoleApplication.cpp
+++ Programowanie/ReadAndWriteOnFilesConsoleApplication/ReadAndWriteOnFilesConsoleApplication.cpp
@@ -1,6 +1,36 @@
 #include <iostream>
+#include <fstream>
+
+void task1()
+{
+    std::string name;
+    int age;
+
+    std::cout << "Podaj imiê:\n";
+    std::cin >> name;
+
+    std::cout << "Podaj swój wiek:\n";
+    std::cin >> age;
+
+    std::ofstream writeInFile;
+    writeInFile.open("c:\\dane.txt");
+
+    if (writeInFile.is_open() == true)
+    {
+        writeInFile << name;
+        writeInFile << "\n";
+        writeInFile << age;
+
+        writeInFile.close();
+    }
+    else
+        std::cout << "B³¹d otwarcia pliku\n";
+}
 
 int main()
 {
-    std::cout << "H";
+    setlocale(LC_CTYPE, "polish");
+    task1();
 }
+
+
