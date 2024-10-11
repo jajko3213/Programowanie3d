2fbfdf60187c669dbe49678705fec2815d005ce7
q <q>
2024-10-11 10:33:40 +0200

    p

--- Programowanie/ReadAndWriteOnFilesConsoleApplication/ReadAndWriteOnFilesConsoleApplication.cpp
+++ Programowanie/ReadAndWriteOnFilesConsoleApplication/ReadAndWriteOnFilesConsoleApplication.cpp
@@ -37,7 +37,7 @@ int main()
 
 /*
 Dane s¹ buforowane, kiedy bufor siê przepe³ni wysy³a dane do pliku.
-Metoda flush wymusza opró¿nienie bufora (bufor sam sie oprózni bez wywo³ywania tej metody).
+Metoda flush wymusza opró¿nienie bufora (bufor sam sie opró¿ni bez wywo³ywania tej metody).
 */
 
 
