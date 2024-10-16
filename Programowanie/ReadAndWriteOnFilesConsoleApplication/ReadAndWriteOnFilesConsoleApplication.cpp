cf771e8bbe6dcc82c1f999fbf1a8f7c8561ab394
q <q>
2024-10-16 08:07:51 +0200

    Dodanie funkcji odczytującej dane z plikui

--- Programowanie/ReadAndWriteOnFilesConsoleApplication/ReadAndWriteOnFilesConsoleApplication.cpp
+++ Programowanie/ReadAndWriteOnFilesConsoleApplication/ReadAndWriteOnFilesConsoleApplication.cpp
@@ -28,15 +28,33 @@ void task1()
         std::cout << "B³¹d otwarcia pliku\n";
 }
 
+//Odczyt zpliku tekstowego
+void task2()
+{
+    std::string name;
+    int age;
+    std::ifstream readFromFile;
+    readFromFile.open("c:\\dane.txt");
+    if (readFromFile.is_open())
+    {
+        readFromFile >> name;
+        readFromFile >> age;
+
+        std::cout << "Imie: " << name << " ,wiek: " << age << "\n";
+        readFromFile.close();
+    }
+}
+
 int main()
 {
     setlocale(LC_CTYPE, "polish");
-    task1();
+    task2();
 }
 
 /*
 Dane s¹ buforowane, kiedy bufor siê przepe³ni wysy³a dane do pliku.
-Metoda flush wymusza opró¿nienie bufora (bufor sam sie opró¿ni bez wywo³ywania tej metody).
+Metoda flush wymusza opró¿nienie bufora.
+Metoda close wywo³uje metode flush.
 */
 
 
