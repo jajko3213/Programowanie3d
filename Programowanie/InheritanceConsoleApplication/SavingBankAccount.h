3887857139af4953d9b6b567465136db56a9ae36
q <q>
2024-11-27 08:23:04 +0100

    Przesłonięcie metod

--- Programowanie/InheritanceConsoleApplication/SavingBankAccount.h
+++ Programowanie/InheritanceConsoleApplication/SavingBankAccount.h
@@ -24,10 +24,9 @@ public:
 };
 
 /*
-
 Modyfikatory dostepu: 
-public - cz³onek jest dostêpny dla wszystkich (zarówno zewnêtrznych klas, jak i w obrêbie tej samej klasy).
-private - Cz³onek jest dostêpny tylko w obrêbie tej klasy (nie jest dostêpny w klasach pochodnych ani spoza klasy).
+public - cz³onek jejest dostêpny tylko w obrst dostêpny dla wszystkich (zarówno zewnêtrznych klas, jak i w obrêbie tej samej klasy).
+private - Cz³onek êbie tej klasy (nie jest dostêpny w klasach pochodnych ani spoza klasy).
 protected - Cz³onek jest dostêpny w obrêbie klasy, a tak¿e w klasach pochodnych 
 (czyli w klasach dziedzicz¹cych), ale nie jest dostêpny dla innych klas.
 
@@ -48,7 +47,15 @@ wpisaæ nazwê konstruktora bazowego z parametrami:
 
 BEZ NAWIASÓW KWADRATOWYCH TO TYLO DLA ZAZNACZENIA CO DO KTÓREGO KONSTRUKTORA!
 Aby wywo³aæ konstruktor parametrowy nale¿y np. SavingBankAccount firstAccount([1000, "Ewa", "z³"] konstruktor bazowy, 0.05);
-Tworz¹c cia³o konstrukotra w pliku cpp nale¿y napisaæ nazwê klasy dwa dwukropki :: i nazwê konstrukotra z parametrami 
+Tworz¹c cia³o konstrukotra w pliku cpp nale¿y napisaæ nazwê klasy dwa dwukropki :: i nazwê konstrukotra z parametrami
+
+Metody mo¿na przes³aniaæ, przes³onienie polega na tym, ¿e w klasie bazowej i dziedzicznej istnieje
+metoda o takim samym nag³ówku oraz zestawie parametrów i gdy wywo³ujemy metodê dla obiektu klasy dziedzicznej
+to wywo³ywana jest metoda, która jest w klasie dziedzicznej. Ale jeœli chcemy wywo³aæ tak¹ sam¹ metodê z
+klasy dziedzicznej i bazowej to nale¿y napisaæ przed metod¹ z klasy nazwê klasy bazowej np.
+void AccountInformation()  klasa dziedziczna(metoda)
+{
+	BankAccount::AccountInformation(); klasa bazowa (metoda) 
+	cout << "hi";
+}
 */
-
-
