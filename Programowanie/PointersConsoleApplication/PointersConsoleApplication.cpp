acaad37c4b3814b0fa54d765466156fb1bff1d0b
q <q>
2025-02-05 09:02:17 +0100

    Działanie skaźników na tablicy, (tablica ukryta zmienna wskaźnikowa), ważne aby wskaźnik był na ten sam typ co zmienna

--- Programowanie/PointersConsoleApplication/PointersConsoleApplication.cpp
+++ Programowanie/PointersConsoleApplication/PointersConsoleApplication.cpp
@@ -14,4 +14,21 @@ int main()
     *ptr = 99;
     std::cout << "firstVar = " << firstVar << "\n";
 
+    bool thirdVar = true;
+
+    bool *boolPtr = &thirdVar;
+
+    int tab[3] = { 2, 98, 4 };
+    std::cout << "tab[0] = " << tab[0] << "\n";
+    std::cout << "tab[0] = " << *tab << "\n"; //*(tab + 0)
+    std::cout << "tab[1] = " << tab[1] << "\n";
+    std::cout << "tab[1] = " << *(tab + 1) << "\n";
+    std::cout << "tab[2] = " << tab[2] << "\n";
+    std::cout << "tab[2] = " << *(tab + 2) << "\n";
+
+    for (int i = 0; i < 3; i++)
+    {
+        std::cout << i << ": " << tab[i] << "\n";
+        std::cout << i << ": " << *(tab + i) << "\n";
+    }
 }
