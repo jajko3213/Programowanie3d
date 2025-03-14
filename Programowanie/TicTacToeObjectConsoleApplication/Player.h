f746249a62453a83a30b4c21510b0c68abb06326
dawid <strzelecki002@wp.pl>
2025-03-14 11:42:27 +0100

    Add tictactoe object console app

---
+++ Programowanie/TicTacToeObjectConsoleApplication/Player.h
@@ -0,0 +1,22 @@
+#pragma once
+class Player
+{
+protected:
+	char symbol;
+
+public:
+	Player(char s)
+	{
+		symbol = s;
+	}
+
+	char GetSymbol()
+	{
+		return symbol;
+	}
+
+	virtual int GetFieldNumber()
+	{
+		return 0;
+	}
+};
---
+++ Programowanie/TicTacToeObjectConsoleApplication/TicTacToeObjectConsoleApplication.cpp
@@ -0,0 +1,51 @@
+﻿#include <iostream>
+#include "HumanPlayer.h"
+#include "TicTacToeBoard.h"
+#include "EasyBoot.h"
+#include "Player.h"
+
+int main()
+{
+	//inicjowanie gry
+	unsigned int playersCount = 3;
+	unsigned int currentPlayerNumber = 0;
+	Player** players = new Player * [playersCount];
+	players[0] = new EasyBoot('O');
+	players[1] = new HumanPlayer('X');
+
+	int fieldNumber;
+
+	unsigned int boardSize = 3;
+	TicTacToeBoard ticTacToeBoard(boardSize);
+
+	ticTacToeBoard.clearBord();
+
+	while (true)
+	{
+		ticTacToeBoard.drawBoard();
+
+		fieldNumber = players[currentPlayerNumber]->GetFieldNumber();
+
+		if (ticTacToeBoard.isNotCorrectMove(fieldNumber))
+			continue;
+
+		ticTacToeBoard.SetBoardField(fieldNumber, players[currentPlayerNumber]->GetSymbol());
+
+		ticTacToeBoard.drawBoard();
+
+		if (ticTacToeBoard.isWin(players[currentPlayerNumber]->GetSymbol()) == true)
+		{
+			std::cout << "Wygrana!!!\n";
+			break;
+		}
+
+		if (ticTacToeBoard.isDraw())
+		{
+			std::cout << "Remis!!!\n";
+			break;
+		}
+
+		//zmiana aktualnego gracza na następnego
+		currentPlayerNumber = (currentPlayerNumber + 1) % playersCount;
+	}
+}
