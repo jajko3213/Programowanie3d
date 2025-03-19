d5bc8ea64d941ff86e7da994115a4b034faef596
q <q>
2025-03-19 08:33:31 +0100

    popr

--- Programowanie/MaturaConsoleApplication/MaturaConsoleApplication.cpp
+++ Programowanie/MaturaConsoleApplication/MaturaConsoleApplication.cpp
@@ -10,7 +10,7 @@ int main()
 	std::ifstream file("przyklad.txt");
 	std::vector<int> numbers;
 	int number;
-	
+
 	while (file >> number)
 	{
 		numbers.push_back(number);
@@ -21,8 +21,8 @@ int main()
 	for (int number : numbers)
 	{
 		std::string str = std::to_string(number);
-		
-		if (str.size() > 0)
+	
+		if (str.length() > 0)
 		{
 			if (str.front() == str.back())
 			{
