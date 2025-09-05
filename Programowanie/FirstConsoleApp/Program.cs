05525cdd51767c0f14417144d3488aa03f758119
Sala213 <Sala213@ezse.pl>
2025-09-05 10:37:18 +0200

    end pierwszych zajęć

--- Programowanie/FirstConsoleApp/Program.cs
+++ Programowanie/FirstConsoleApp/Program.cs
@@ -1,2 +1,5 @@
-﻿Console.Write("Hello, World!");
+﻿Console.WriteLine("Hello, World!");
 Console.WriteLine("Hello, World!");
+string name = "Jan";
+string surname = "Kowalski";
+Console.WriteLine("Witaj " + name + " " + surname + " tutaj!");
