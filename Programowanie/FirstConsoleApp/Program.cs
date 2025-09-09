762d36bfb0794034280ac0bf01d4bd63884041d6
Dawid Strzelecki <strzelecki002@wp.pl>
2025-09-09 13:56:01 +0200

    Poznawanie metody writeline

--- Programowanie/FirstConsoleApp/Program.cs
+++ Programowanie/FirstConsoleApp/Program.cs
@@ -5,3 +5,6 @@ string surname = "Kowalski";
 Console.WriteLine("Witaj " + name + " " + surname + " tutaj!");
 Console.WriteLine("Witaj {0} {1} tutaj!", name, surname);
 Console.WriteLine($"Witaj {name} {surname} tutaj");
+//znak dolara powoduje to że możemy używać {} do wstawiania zmiennych
+Console.WriteLine(@"Prękość to km\h");
+//znak @ powoduje że nie trzeba używać podwójnych \\ do ścieżek lub innych rzeczy
