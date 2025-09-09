10b7a696df3e29c2ee1e037f8ec5506cadb890cb
Dawid Strzelecki <strzelecki002@wp.pl>
2025-09-09 13:57:03 +0200

    .

--- Programowanie/FirstConsoleApp/Program.cs
+++ Programowanie/FirstConsoleApp/Program.cs
@@ -7,4 +7,4 @@ Console.WriteLine("Witaj {0} {1} tutaj!", name, surname);
 Console.WriteLine($"Witaj {name} {surname} tutaj");
 //znak dolara powoduje to że możemy używać {} do wstawiania zmiennych
 Console.WriteLine(@"Prękość to km\h");
-//znak @ powoduje że nie trzeba używać podwójnych \\ do ścieżek lub innych rzeczy
+//znak @ powoduje że nie trzeba znaków specjalnych
