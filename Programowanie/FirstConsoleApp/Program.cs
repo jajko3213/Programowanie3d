9bfbccbe84fbe6b83fe32486316f08faa727ea52
Dawid Strzelecki <strzelecki002@wp.pl>
2025-09-09 13:28:26 +0200

    Poznawanie metody WriteLine

--- Programowanie/FirstConsoleApp/Program.cs
+++ Programowanie/FirstConsoleApp/Program.cs
@@ -2,4 +2,6 @@
 Console.WriteLine("Hello, World!");
 string name = "Jan";
 string surname = "Kowalski";
-Console.WriteLine("Witaj " + name + " " + surname + " tutaj!"); 
+Console.WriteLine("Witaj " + name + " " + surname + " tutaj!");
+Console.WriteLine("Witaj {0} {1} tutaj!", name, surname);
+Console.WriteLine($"Witaj {name} {surname} tutaj");
