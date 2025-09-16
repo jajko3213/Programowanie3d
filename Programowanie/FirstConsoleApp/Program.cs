0e5d2e53538bc3a102fa166bef234ea24ab4a02c
Sala213 <Sala213@ezse.pl>
2025-09-16 13:50:09 +0200

    s

--- Programowanie/FirstConsoleApp/Program.cs
+++ Programowanie/FirstConsoleApp/Program.cs
@@ -3,5 +3,3 @@ Console.WriteLine("Hello, World!");
 string name = "Jan";
 string surname = "Kowalski";
 Console.WriteLine("Witaj " + name + " " + surname + " tutaj!");
-Console.WriteLine("Witaj {0} {1} tutaj!", name, surname);
-Console.WriteLine($"Witaj {name} {surname} tutaj!")
