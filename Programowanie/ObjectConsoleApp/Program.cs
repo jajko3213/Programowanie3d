f10b7b07f960a9bff1f9f4d83be6776d57fe060f
Sala213 <Sala213@ezse.pl>
2025-09-16 14:30:23 +0200

    Struktury

---
+++ Programowanie/ObjectConsoleApp/Program.cs
@@ -0,0 +1,35 @@
+﻿using ObjectConsoleApp;
+
+pointStruct pointStruct;
+pointStruct.x = 5;
+pointStruct.y = 10; 
+
+Console.WriteLine($"Punkt ma: x = {pointStruct.x}, y = {pointStruct.y}");
+
+pointStruct secondPointStruct = pointStruct;
+secondPointStruct.y = 20;
+
+Console.WriteLine($"Punkt ma: x = {pointStruct.x}, y = {pointStruct.y}");
+Console.WriteLine($"Drugi punkt ma: x = {secondPointStruct.x}, y = {secondPointStruct.y}");
+
+void ParameterTestStruct_v1(pointStruct point)
+{
+    Console.WriteLine($"W ParameterTeststruct_v1 ma: x = {point.x}, y = {point.y}");
+    point.x = 99;
+    Console.WriteLine($"W ParameterTeststruct_v1 ma: x = {point.x}, y = {point.y}");
+}
+
+ParameterTestStruct_v1(pointStruct);
+Console.WriteLine($"Punkt po wywołaniu ma: x = {pointStruct.x}, y = {pointStruct.y}");
+
+Console.WriteLine("---------------------------");
+
+void ParameterTestStruct_v2(ref pointStruct point)
+{
+    Console.WriteLine($"W ParameterTeststruct_v2 ma: x = {point.x}, y = {point.y}");
+    point.x = 99;
+    Console.WriteLine($"W ParameterTeststruct_v2 ma: x = {point.x}, y = {point.y}");
+}
+
+ParameterTestStruct_v2(ref pointStruct);
+Console.WriteLine($"Punkt po wywołaniu ma: x = {pointStruct.x}, y = {pointStruct.y}");
