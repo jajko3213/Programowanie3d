21aff28f89482c3baba403a4df3154c3b4aa0158
Sala213 <Sala213@ezse.pl>
2025-09-23 08:11:47 +0200

    Properties

---
+++ Programowanie/AboutObjectConsoleApp/Point.cs
@@ -0,0 +1,63 @@
+﻿namespace AboutObjectConsoleApp
+{
+    internal class Point
+    {
+        private int x;
+        private int y;
+
+        /*
+        Properties - łączy pole i metode w jedno, odwołujemy się jak do pola ale 
+        działa jak metoda
+        */
+
+        public int X
+        {
+            set
+            {
+                x = value;
+            }
+
+            get
+            {
+                return x;
+            }
+        }
+
+        public Point()
+        {
+            x = 0; 
+            y = 0;
+        }
+
+        public Point(int x, int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+
+        public void SetX(int x)
+        {
+            this.x = x;
+        }
+
+        public void SetY(int y)
+        {
+            this.y = y;
+        }
+
+        public int GetX()
+        {
+            return x;
+        }
+
+        public int GetY()
+        {
+            return y;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"{x}, {y}");
+        }
+    }
+}
---
+++ Programowanie/AboutObjectConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using AboutObjectConsoleApp;
+
+Point firstPoint = new Point();
+firstPoint.ShowInfo();
+
+firstPoint.SetX(10);
+firstPoint.SetY(10);
+
+firstPoint.ShowInfo();
+
+//Można usunąć w drugiej części nazwę klasy
+Point secondPoint = new(2, 4);
+secondPoint.ShowInfo();
+
+//int value = secondPoint.GetX();
+//value = secondPoint.x;
+int value = secondPoint.X;
+Console.WriteLine(value);
+secondPoint.X = 20;
+secondPoint.ShowInfo();
