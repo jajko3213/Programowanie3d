4e38bc6a586e6cbec4275a3c578ec189a1ac9e56
Dawid Strzelecki <strzelecki002@wp.pl>
2025-10-20 20:38:39 +0200

    Dodanie gezaminu ze stycznia2023_1 aplikacja mobilna. Zakończenie foldery Sty2023_1

--- Programowanie/PracticalTaskConsoleApp/Program.cs
+++ Programowanie/PracticalTaskConsoleApp/Program.cs
@@ -1,4 +1,15 @@
 ﻿using PracticalTaskConsoleApp.Styczeń2023_1;
 
-Task1 task1 = new Task1();
-task1.Task();
+//Task1 task1 = new Task1();
+//task1.Task();
+
+Notatka notatkaPierwsza = new Notatka("Zakupy", "Kupić mleko, chleb i jajka.");
+Notatka notatkaDruga = new Notatka("Praca", "Zakończyć raport do piątku.");
+
+Console.WriteLine("Pierwsza notatka:");
+notatkaPierwsza.WyswietlNotatke();
+notatkaPierwsza.Diagnostyczna();
+
+Console.WriteLine("\nDruga notatka:");
+notatkaDruga.WyswietlNotatke();
+notatkaDruga.Diagnostyczna();
---
+++ Programowanie/PracticalTaskConsoleApp/Styczeń2023_1/Notatka.cs
@@ -0,0 +1,39 @@
+﻿namespace PracticalTaskConsoleApp.Styczeń2023_1
+{
+    /**********************************************
+    klasa: Notatka
+    opis: Klasa notatka reprezentuje prosty system do tworzenia notatek, 
+    które posiadają id, tytuł oraz treść
+    pola: 
+    *id - pole prywatne przechowywujące unikalny identyfikator notatki, pole jest typu int
+    *tytul - pole chronione przechowywujące tytuł notatki, pole jest typu string
+    *tresc - pole chronione przechowywujące treść notatki, pole jest typu string
+    autor: Dawid Strzelecki
+    ***********************************************/
+
+    internal class Notatka
+    {
+        private static int licznikNotatek = 0;
+        private int id;
+        protected string tytul;
+        protected string tresc; 
+
+        public Notatka(string tytul, string tresc)
+        {
+            licznikNotatek++;
+            this.id = licznikNotatek;
+            this.tytul = tytul;
+            this.tresc = tresc;
+        }
+
+        public void WyswietlNotatke()
+        {
+            Console.WriteLine($"Tytuł: {tytul}\nTreść: {tresc}");
+        }
+
+        public void Diagnostyczna()
+        {
+            Console.WriteLine($"ID: {id}; Tytuł: {tytul}; Treść: {tresc}");
+        }
+    }
+}
